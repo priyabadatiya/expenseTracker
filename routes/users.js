d8eda8bd04127fec8a173083b8b8ca737a7eac72
priyabadatiya <priyabadatiya@gmail.com>
2021-08-13 18:13:54 +0530

    update routes

--- routes/users.js
+++ routes/users.js
@@ -48,7 +48,8 @@ router.post('/register', (req, res, next) => {
           <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Expense Tracker</a>
         </div>
         <p style="font-size:1.1em">Hi,</p>
-        <p>Thanks for signing up with Expense Tracker! You must follow this link with the following Verification Code to to activate your account: https://expense-tracker-by-piku.herokuapp.com/users/verifyEmail </p>
+        <p>Thanks for signing up with Expense Tracker! You must follow this link with the following Verification Code to activate your account: </p>
+        <p> https://expense-tracker-by-piku.herokuapp.com/users/verifyEmail </p>
         <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${req.body.random}</h2>
         <p style="font-size:0.9em;">Regards,<br />Priyanka Badatiya</p>
         <hr style="border:none;border-top:1px solid #eee" />
